3753b600a98f503ffef88096c63c37dde0d255c5
Eugeniy Mikhailov <evgmik@gmail.com>
2013-09-16 23:59:02 -0400

    basic filling for motion class

---
+++ eem/motion/basicMotion.java
@@ -0,0 +1,21 @@
+// -*- java -*-
+
+package eem.motion;
+
+import eem.EvBot;
+import eem.target.*;
+import eem.misc.*;
+import java.util.Random;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+
+public class basicMotion {
+	protected EvBot myBot;
+
+	public basicMotion(EvBot bot) {
+		myBot = bot;
+	};
+
+}
