080685fbff119c4ce073d4fa36eb6a2575d9ebd2
Eugeniy Mikhailov <evgmik@gmail.com>
2013-09-19 12:20:19 -0400

    renaming of log levels names

--- eem/misc/logger.java
+++ eem/misc/logger.java
@@ -4,11 +4,11 @@ package eem.misc;
 public class logger {
 	// logger staff
 	// debug levels
-	public final static int dbg_important=0;
-	public final static int dbg_routine=5;
-	public final static int dbg_debuging=6;
-	public final static int dbg_noise=10;
-	static int verbosity_level=6; // current level, smaller is less noisy
+	public final static int log_important=0;
+	public final static int log_routine=5;
+	public final static int log_debuging=6;
+	public final static int log_noise=10;
+	static int verbosity_level=log_debuging; // current level, smaller is less noisy
 
 	public logger (int vLevel) {
 		verbosity_level = vLevel;
@@ -23,15 +23,15 @@ public class logger {
 	}
 
 	public static void noise(String s) {
-		log_message(dbg_noise, s);
+		log_message(log_noise, s);
 	}
 
 	public static void dbg(String s) {
-		log_message(dbg_debuging, s);
+		log_message(log_debuging, s);
 	}
 
 	public static void routine(String s) {
-		log_message(dbg_routine, s);
+		log_message(log_routine, s);
 	}
 }
 
