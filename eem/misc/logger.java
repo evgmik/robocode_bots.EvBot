31bcfa287100bf445293beec5534d42347587b59
Eugeniy Mikhailov <evgmik@gmail.com>
2013-09-18 21:43:44 -0400

    proper implementation of the logger

--- eem/misc/logger.java
+++ eem/misc/logger.java
@@ -4,19 +4,30 @@ package eem.misc;
 public class logger {
 	// logger staff
 	// debug levels
-	public final int dbg_important=0;
-	public final int dbg_rutine=5;
-	public final int dbg_debuging=6;
-	public final int dbg_noise=10;
-	int verbosity_level=6; // current level, smaller is less noisy
+	public final static int dbg_important=0;
+	public final static int dbg_rutine=5;
+	public final static int dbg_debuging=6;
+	public final static int dbg_noise=10;
+	static int verbosity_level=6; // current level, smaller is less noisy
 
 	public logger (int vLevel) {
 		verbosity_level = vLevel;
 	}
 
-	public void dbg(int level, String s) {
+	public logger () {
+	}
+
+	public static void log_message(int level, String s) {
 		if (level <= verbosity_level)
 			System.out.println(s);
+	}
+
+	public static void noise(String s) {
+		log_message(dbg_noise, s);
+	}
+
+	public static void dbg(String s) {
+		log_message(dbg_debuging, s);
 	}
 }
 
