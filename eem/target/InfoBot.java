683b7b0bc53375b3940067efa614d1890396ae13
Eugeniy Mikhailov <evgmik@gmail.com>
2013-10-04 22:48:15 -0400

    target class split to base InfoBot and Target
    This should simplify tracking of other bot stats

---
+++ eem/target/InfoBot.java
@@ -0,0 +1,135 @@
+// -*- java -*-
+
+package eem.target;
+
+import eem.target.botStatPoint;
+import eem.misc.*;
+import java.awt.geom.Point2D;
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+public class InfoBot {
+	private int nonexisting_coord = -10000;
+	private long far_ago  = -10000;
+	private botStatPoint statLast;
+	private botStatPoint statPrev;
+	private String name = "";
+
+	private static int bulletHitCount = 0;
+	private static int bulletFiredCount = 0;
+
+	public InfoBot() {
+		statPrev = new botStatPoint( new Point2D.Double( nonexisting_coord, nonexisting_coord), far_ago);
+		statLast = new botStatPoint( new Point2D.Double( nonexisting_coord, nonexisting_coord), far_ago);
+	}
+	
+	public int getBulletFiredCount() {
+		return this.bulletFiredCount;
+	}
+
+	public int getBulletHitCount() {
+		return this.bulletHitCount;
+	}
+
+	protected void incBulletFiredCount() {
+		this.bulletFiredCount++;
+	}
+
+	public void incBulletHitCount() {
+		this.bulletHitCount++;
+	}
+
+	public double getGunHitRate() {
+		return (this.getBulletHitCount() ) / (this.getBulletFiredCount() + 1.0);
+	}
+
+	public void printGunsStats() {
+		logger.routine("Enemy gun hit rate = " + this.getGunHitRate() );
+	}
+
+	public void initTic(long ticTime) {
+		if ( didItFireABullet() ) {
+			this.incBulletFiredCount();
+		}
+	}
+
+	public InfoBot update(Point2D.Double pos, long tStamp) {
+		statPrev = statLast;
+		statLast = new botStatPoint(pos, tStamp);
+		return this;
+	}
+
+	public InfoBot update(botStatPoint statPnt) {
+		statPrev = statLast;
+		statLast = statPnt;
+		return this;
+	}
+
+	public double getEnergy() {
+		return statLast.getEnergy();
+	}
+
+	public Point2D.Double getVelocity() {
+		return statLast.getVelocity();
+	}
+
+	public void setName(String n) {
+		name = n;
+	}
+
+	public double getLastDistance(Point2D.Double p) {
+		return  statLast.getDistance(p);
+	}
+
+	public double getX() {
+		return  statLast.getX();
+	}
+
+	public double getY() {
+		return  statLast.getY();
+	}
+
+	public Point2D.Double getPosition() {
+		return  statLast.getPosition();
+	}
+
+	public long getLastSeenTime() {
+		return  statLast.getTimeStamp();
+	}
+
+
+	public double energyDrop() {
+		return  statPrev.getEnergy() - statLast.getEnergy();
+	}
+
+	public boolean didItFireABullet() {
+		boolean stat = true;
+		double eDrop = energyDrop();
+		if ( (eDrop < .1) || (3 < eDrop) ) {
+			stat=false;
+			logger.noise("enemy did not fired a bullet");
+		}
+		return stat;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public String format() {
+		String str;
+		str = "Target bot name: " + getName() + "\n";
+		str = str + "Last: " + statLast.format() + "\n" + "Prev: " + statPrev.format();
+		return str;
+	}
+
+	public void onPaint(Graphics2D g) {
+			g.setColor(new Color(0xff, 0x00, 0x00, 0x80));
+
+			// Draw a filled square on top of the scanned robot that covers it
+			g.fillRect((int)statLast.getX() - 20, (int)statLast.getY() - 20, 40, 40);
+	}
+
+}
+
--- eem/target/target.java
+++ eem/target/target.java
@@ -8,48 +8,15 @@ import java.awt.geom.Point2D;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-public class target {
-	private int nonexisting_coord = -10000;
-	private long far_ago  = -10000;
-	private botStatPoint statLast;
-	private botStatPoint statPrev;
-	private String name = "";
+public class target extends InfoBot {
 	public boolean haveTarget=false;
 	public boolean targetUnlocked = false; 
 
-	private static int bulletHitCount = 0;
-	private static int bulletFiredCount = 0;
-
 	public target() {
-		statPrev = new botStatPoint( new Point2D.Double( nonexisting_coord, nonexisting_coord), far_ago);
-		statLast = new botStatPoint( new Point2D.Double( nonexisting_coord, nonexisting_coord), far_ago);
-		haveTarget=false;
+		haveTarget = false;
+		targetUnlocked = false;
 	}
 	
-	public int getBulletFiredCount() {
-		return this.bulletFiredCount;
-	}
-
-	public int getBulletHitCount() {
-		return this.bulletHitCount;
-	}
-
-	protected void incBulletFiredCount() {
-		this.bulletFiredCount++;
-	}
-
-	public void incBulletHitCount() {
-		this.bulletHitCount++;
-	}
-
-	public double getGunHitRate() {
-		return (this.getBulletHitCount() ) / (this.getBulletFiredCount() + 1.0);
-	}
-
-	public void printGunsStats() {
-		logger.routine("Enemy gun hit rate = " + this.getGunHitRate() );
-	}
-
 	public void initTic(long ticTime) {
 		// updating UnLocked status
 		if ( ( ticTime - this.getLastSeenTime() ) > 2) 
@@ -57,93 +24,23 @@ public class target {
 		else
 			this.setUnLockedStatus(false);
 
-		if ( didItFireABullet() ) {
-			this.incBulletFiredCount();
-		}
-	}
-
-	public void setUnLockedStatus(boolean val) {
-		targetUnlocked = val;
+		super.initTic(ticTime);
 	}
 
 	public target update(Point2D.Double pos, long tStamp) {
-		statPrev = statLast;
-		statLast = new botStatPoint(pos, tStamp);
+		super.update( pos, tStamp );
 		haveTarget = true;
 		return this;
 	}
 
 	public target update(botStatPoint statPnt) {
-		statPrev = statLast;
-		statLast = statPnt;
+		super.update( statPnt );
 		haveTarget = true;
 		return this;
 	}
 
-	public double getEnergy() {
-		return statLast.getEnergy();
-	}
-
-	public Point2D.Double getVelocity() {
-		return statLast.getVelocity();
-	}
-
-	public void setName(String n) {
-		name = n;
-	}
-
-	public double getLastDistance(Point2D.Double p) {
-		return  statLast.getDistance(p);
-	}
-
-	public double getX() {
-		return  statLast.getX();
-	}
-
-	public double getY() {
-		return  statLast.getY();
-	}
-
-	public Point2D.Double getPosition() {
-		return  statLast.getPosition();
-	}
-
-	public long getLastSeenTime() {
-		return  statLast.getTimeStamp();
-	}
-
-
-	public double energyDrop() {
-		return  statPrev.getEnergy() - statLast.getEnergy();
-	}
-
-	public boolean didItFireABullet() {
-		boolean stat = true;
-		double eDrop = energyDrop();
-		if ( (eDrop < .1) || (3 < eDrop) ) {
-			stat=false;
-			logger.noise("enemy did not fired a bullet");
-		}
-		return stat;
-	}
-
-
-	public String getName() {
-		return name;
-	}
-
-	public String format() {
-		String str;
-		str = "Target bot name: " + getName() + "\n";
-		str = str + "Last: " + statLast.format() + "\n" + "Prev: " + statPrev.format();
-		return str;
-	}
-
-	public void onPaint(Graphics2D g) {
-			g.setColor(new Color(0xff, 0x00, 0x00, 0x80));
-
-			// Draw a filled square on top of the scanned robot that covers it
-			g.fillRect((int)statLast.getX() - 20, (int)statLast.getY() - 20, 40, 40);
+	public void setUnLockedStatus(boolean val) {
+		targetUnlocked = val;
 	}
 
 }
