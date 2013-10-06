1e66ad97dc754b12d8f3e52616cccac73b30e1b8
Eugeniy Mikhailov <evgmik@gmail.com>
2013-10-05 22:27:56 -0400

    simplified java packages dependencies

--- eem/bullets/bulletsManager.java
+++ eem/bullets/bulletsManager.java
@@ -10,9 +10,6 @@ import java.util.Random;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.*;
 
 
