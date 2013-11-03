8b9afd44fcc01ec3be7d180b2f21564dcdf4cd6a
Eugeniy Mikhailov <evgmik@gmail.com>
2013-11-03 15:57:17 -0500

    Added gun to generate shadow bullets

---
+++ eem/gun/shadowGun.java
@@ -0,0 +1,20 @@
+// -*- java -*-
+
+package eem.gun;
+
+import eem.EvBot;
+import eem.target.*;
+import eem.misc.*;
+import eem.gun.misc;
+import java.awt.Color;
+import java.awt.geom.Point2D;
+import robocode.util.*;
+
+public class shadowGun extends baseGun {
+
+	public shadowGun() {
+		gunName = "shadow";
+		gunColor = Color.green;
+	}
+
+}
