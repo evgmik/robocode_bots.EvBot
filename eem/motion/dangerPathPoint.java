9b4843371534d00546ee0adab1eeb60a342e5720
Eugeniy Mikhailov <evgmik@gmail.com>
2014-04-05 15:22:16 -0400

    expenden dangerPathPoint

--- eem/motion/dangerPathPoint.java
+++ eem/motion/dangerPathPoint.java
@@ -11,13 +11,35 @@ import java.awt.geom.Point2D;
 
 public class dangerPathPoint extends dangerPoint {
 	private double accelDir = 0; // how to accelerate to get here
+	private double turnAngle = 0; // how to rotate to get here
+	private double velocity = 0; // at this point
+	private double heading = 0;  // at this point
 	
-	public dangerPathPoint( Point2D.Double dP, double dangerLevel, double accelDir) {
+	public dangerPathPoint( Point2D.Double dP, double dangerLevel, double turnAngle, double accelDir) {
 		super( dP, dangerLevel );
 		this.accelDir = accelDir;
+		this.turnAngle = turnAngle;
+	}
+
+	public dangerPathPoint( Point2D.Double dP, double dangerLevel, double turnAngle, double accelDir, double velocity, double heading) {
+		this( dP, dangerLevel, turnAngle, accelDir );
+		this.velocity = velocity;
+		this.heading = heading;
+	}
+
+	public double getHeading() {
+		return heading;
+	}
+
+	public double getVelocity() {
+		return velocity;
 	}
 
 	public double getAccelDir() {
 		return accelDir;
+	}
+
+	public double getTurnAngle() {
+		return turnAngle;
 	}
 }
