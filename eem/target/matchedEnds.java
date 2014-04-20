e253d344b2261ee67e00d89ea8ac7d281e216c1b
Eugeniy Mikhailov <evgmik@gmail.com>
2014-04-19 23:06:20 -0400

    added new class to keep pif matched ends hierarhy

---
+++ eem/target/matchedEnds.java
@@ -0,0 +1,46 @@
+// -*- java -*-
+
+package eem.target;
+
+import eem.EvBot;
+import eem.target.*;
+import eem.misc.*;
+import java.awt.geom.Point2D;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.util.*;
+import java.lang.Integer;
+import robocode.*;
+
+public class matchedEnds extends LinkedList<LinkedList<Integer>> {
+	// special class to keep list of lists of pattern marched ends for a given InfoBot
+	// each next list keeps ends with list # of pattern points
+	
+	//LinkedList<LinkedList<Integer>> endsListVsPatternLength = new LinkedList<LinkedList<Integer>>();
+	public matchedEnds() {
+		super();
+	}
+
+	public LinkedList<Integer> flatten() { 
+		LinkedList<Integer> endIndexes = new LinkedList<Integer>();
+		for ( LinkedList<Integer> l : this ) {
+			for ( int i : l ) {
+				endIndexes.add(i);
+			}
+		}
+		return endIndexes;
+	}
+	
+	public String format() {
+		String outStr = "List of matched ends has " + this.size() + " depth";
+		int cnt = 0;
+		for ( LinkedList<Integer> l : this ) {
+			cnt++;
+			outStr += "\n";
+			outStr += " for pattern length = " + cnt + " find " + l.size() + " matches";
+			outStr += "\n";
+			outStr +=("  matches = " + l );
+		}
+		return outStr;
+	}
+}
