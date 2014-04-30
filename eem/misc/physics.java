94e0125928c55b6b281ea79d07a6eef54ba7c603
Eugeniy Mikhailov <evgmik@gmail.com>
2014-04-30 12:01:04 -0400

    added gun cooling time calculation

--- eem/misc/physics.java
+++ eem/misc/physics.java
@@ -9,9 +9,16 @@ import eem.EvBot;
 public class physics {
 	public static int robotHalfSize = 0;
 	public static Point2D.Double BattleField = new Point2D.Double(0,0);
+	public static double coolingRate = 0.1; 
 
 	public static void init(EvBot myBot) {
 		robotHalfSize = myBot.robotHalfSize;
 		BattleField = (Point2D.Double) myBot.BattleField.clone();
+		coolingRate = myBot.getGunCoolingRate();
 	}
+
+	public static int gunCoolingTime( double heat ) {
+		return (int) Math.ceil( heat/coolingRate );
+	}
+
 }
